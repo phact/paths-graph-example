10b757b4fc49be18780afd9e3b7480e42968e2c0
denisekgosnell <denisegosnell@gmail.com>
2018-06-05 15:51:44 -0400

    PATH-GRAPH: initial working structure

--- ebdse/runebdse.sh
+++ ebdse/runebdse.sh
@@ -3,13 +3,15 @@
 set -x
 
 #Vs
-person=4000
-application=400
-account=4000
+person=15000
+application=1500
+creditCard=1500
+device=1500
+account=15000
 event=40000
 
 #Es
-familyMember=10
+familyMember=1000
 submittedApp=400
 listedOnApp=400
 ownsAccount=4000
@@ -37,6 +39,15 @@ reads=100
 /tmp/ebdse/ebdse run type=dsegraph yaml=paths graphname=$graphname tags=phase:graph-schema cycles=1 host=$host
 
 # The rest of these are <<>> in the yaml
-/tmp/ebdse/ebdse run type=dsegraph yaml=paths graphname=$graphname tags=phase:add-familyMember-edge cycles=$familyMember person=$person cyclerate=$rate host=$host threads=$threads
 
-/tmp/ebdse/ebdse run type=dsegraph yaml=paths graphname=$graphname tags=phase:add-ownsAccount-edge cycles=$ownsAccount account=$account person=$person cyclerate=$rate host=$host threads=$threads
+# add initial star graphs of person with accounts, cards, and devices
+/tmp/ebdse/ebdse run type=dsegraph yaml=paths graphname=$graphname tags=phase:add-ownsAccount-edge cycles=$ownsAccount cyclerate=$rate host=$host threads=$threads account=$account person=$person application=$application creditCard=$creditCard device=$device event=$event
+
+# connect family members
+/tmp/ebdse/ebdse run type=dsegraph yaml=paths graphname=$graphname tags=phase:add-familyMember-edge cycles=$familyMember cyclerate=$rate host=$host threads=$threads account=$account person=$person application=$application creditCard=$creditCard device=$device event=$event
+
+# add applications between people
+/tmp/ebdse/ebdse run type=dsegraph yaml=paths graphname=$graphname tags=phase:add-submittedApp-edge cycles=$application cyclerate=$rate host=$host threads=$threads account=$account person=$person application=$application creditCard=$creditCard device=$device event=$event
+
+# add events between accounts
+/tmp/ebdse/ebdse run type=dsegraph yaml=paths graphname=$graphname tags=phase:add-event-edge cycles=$event cyclerate=$rate host=$host threads=$threads account=$account person=$person application=$application creditCard=$creditCard device=$device event=$event

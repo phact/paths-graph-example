45f448d1c54b028acc74d5ceb46073025fed0da8
denisekgosnell <denisegosnell@gmail.com>
2018-06-22 11:53:39 -0400

    prepping for a large run

--- ebdse/runebdse.sh
+++ ebdse/runebdse.sh
@@ -3,20 +3,20 @@
 set -x
 
 #Vs
-person=500000
-account=500000       # same as person
-application=25000    # 5% of person
-creditCard=375000    # 75% of person
-device=375000        # 75% of person
-event=2000000        # 4x person
+person=500000000
+account=500000000       # same as person
+application=25000000    # 5% of person
+creditCard=375000000    # 75% of person
+device=375000000        # 75% of person
+event=2000000000        # 4x person
 
 #Es
-familyMember=125000  # 25% of person
-submittedApp=25000   # same as application
-listedOnApp=500000   # same as account
-ownsAccount=500000   # same as account
-transferTo=2000000   # same as event
-transferFrom=2000000 # same as event
+familyMember=125000000  # 25% of person
+submittedApp=25000000   # same as application
+listedOnApp=500000000   # same as account
+ownsAccount=500000000   # same as account
+transferTo=2000000000   # same as event
+transferFrom=2000000000 # same as event
 
 #misc
 rate=10k
